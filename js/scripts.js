c2b87df8be639235ae2b6004e766d3f116b00eb6
Matthew Neary <mattneary1@gmail.com>
2022-09-03 08:48:46 -0700

    Week 4 Assignment

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 $(function() {
   $(".carousel").carousel( { interval: 2000 } );
-  $("#carouselButton").click(function(){
+  $("#carouselButton").click(function() {
       if ($("#carouselButton").children("i").hasClass("fa-pause")) {
           $(".carousel").carousel("pause");
           $("#carouselButton").children("i").removeClass("fa-pause");
@@ -12,3 +12,12 @@ $(function() {
       }
   });
 });
+
+$("#reserveButton").click(function() {
+    $("#reserveModal").modal('show');
+});
+
+$("#loginButton").click(function() {
+    $("#loginModal").modal('show');
+});
+
